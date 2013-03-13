46a279f62d62b362935fb39bcce6711ef8c1d785
bordicon <borden@nanosatisfi.com>
2013-03-13 01:54:11 -0700

    clean and refactor

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 CACHE_DIR=Symfony/app/cache
 LOG_DIR=Symfony/app/logs
+
+# INSTALL PACKAGES
+apt-get install apache2 php5-sqlite php-apc php5-intl php-pear php5-dev
+pecl install mongo-1.2.12
+
+# UPDATE CONFS
 echo date.timezone = America/Los_Angeles >> /etc/php5/apache2/php.ini
 echo date.timezone = America/Los_Angeles >> /etc/php5/cli/php.ini
 echo extension=mongo.so >> /etc/php5/cli/php.ini
 echo extension=mongo.so >> /etc/php5/apache/php.ini
-apt-get install apache2 php5-sqlite php-apc php5-intl php-pear php5-dev
+
+# INITIALIZING APP STRUCTURE
 if [ ! -d $CACHE_DIR ]; then
 	mkdir -p $CACHE_DIR
 	chown $SUDO_USER:$SUDO_USER $CACHE_DIR
@@ -18,7 +25,6 @@ if [ ! -d $LOG_DIR ]; then
 fi
 chmod 777 -R $LOG_DIR
 
-pecl install mongo-1.2.12
-
+# INSTALL SYMFONY BUNDLES
 cd Symfony && php bin/vendors install --reinstall && \
 	chown -R $SUDO_USER:$SUDO_USER vendor && cd -
