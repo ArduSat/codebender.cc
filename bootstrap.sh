58300717ea14285513aa1713ec9b013e65f86f06
bordicon <borden@nanosatisfi.com>
2013-03-13 01:26:10 -0700

    update deps and boot script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+CACHE_DIR=Symfony/app/cache
+LOG_DIR=Symfony/app/logs
+# echo date.timezone = America/Los_Angeles >> /etc/php5/apache2/php.ini
+# echo date.timezone = America/Los_Angeles >> /etc/php5/cli/php.ini
+apt-get install apache2 php5-sqlite php-apc php5-intl
+if [ ! -d $CACHE_DIR ]; then
+	mkdir -p $CACHE_DIR
+	chown $USER:$USER $CACHE_DIR
+fi
+chmod 777 -R $CACHE_DIR
 
-sudo su
-echo date.timezone = America/Los_Angeles
-sudo apt-get install apache
-
+if [ ! -d $LOG_DIR ]; then
+	mkdir -p $LOG_DIR
+	chown $USER:$USER $LOG_DIR
+fi
+chmod 777 -R $LOG_DIR
