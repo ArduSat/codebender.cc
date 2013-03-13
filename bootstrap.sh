16a7f6ab0e08125bf48764d93ac4a4ec1a248295
bordicon <borden@nanosatisfi.com>
2013-03-13 01:50:56 -0700

    added pecl and mongodb ext to php

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,15 +3,20 @@ CACHE_DIR=Symfony/app/cache
 LOG_DIR=Symfony/app/logs
 # echo date.timezone = America/Los_Angeles >> /etc/php5/apache2/php.ini
 # echo date.timezone = America/Los_Angeles >> /etc/php5/cli/php.ini
-apt-get install apache2 php5-sqlite php-apc php5-intl
+apt-get install apache2 php5-sqlite php-apc php5-intl php-pear php5-dev
 if [ ! -d $CACHE_DIR ]; then
 	mkdir -p $CACHE_DIR
-	chown $USER:$USER $CACHE_DIR
+	chown $SUDO_USER:$SUDO_USER $CACHE_DIR
 fi
 chmod 777 -R $CACHE_DIR
 
 if [ ! -d $LOG_DIR ]; then
 	mkdir -p $LOG_DIR
-	chown $USER:$USER $LOG_DIR
+	chown $SUDO_USER:$SUDO_USER $LOG_DIR
 fi
 chmod 777 -R $LOG_DIR
+
+pecl install mongo-1.2.12
+
+cd Symfony && php bin/vendors install --reinstall && \
+	chown -R $SUDO_USER:$SUDO_USER vendor && cd -
